7f055a332a88267ced063ea40bab944a6fe3ed35
Alex <alexanderrg3003@gmail.com>
2022-12-02 23:13:40 +0100

    android studio commit 4

--- app/src/main/java/com/example/buscaminas_reyes/MainActivity.java
+++ app/src/main/java/com/example/buscaminas_reyes/MainActivity.java
@@ -87,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
     ;
 
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         findViewById(R.id.action_settings);
 
